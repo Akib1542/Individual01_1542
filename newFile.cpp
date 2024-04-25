ae19a9c1320e9559433b1ea571da310041b9377e
Akib <ali.haider@brainstation-23.com>
2024-04-25 12:12:45 +0600

    segmentTree.cpp file stashed into feature branch successfully

---
+++ newFile.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Using for Stash practice!"<<endl;
+    
+}
---
+++ segmentTree.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Using for Stash practice!"<<endl;
+
+    cout<<"Segment Tree file changed"<<endl;
+    
+}
